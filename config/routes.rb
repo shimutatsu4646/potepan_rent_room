4547857fb825250eabfd75bff094efd224b75c78
Ubuntu <ubuntu@ip-172-31-35-1.ap-northeast-1.compute.internal>
2021-08-30 00:53:01 +0000

    routes.rbの更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  get 'reservations/index'
-  get 'reservations/show'
-  get 'inns/index'
-  get 'inns/show'
-  get 'inns/new'
-  get 'inns/edit'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
+  # get 'reservations/index'
+  # get 'reservations/show'
+  # get 'inns/index'
+  # get 'inns/show'
+  # get 'inns/new'
+  # get 'inns/edit'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/edit'
+  
+  # root 'inns/search'
+  get 'reservations/confirm', to: 'reservations#confirm'
+  
+  resources :users, except: [:index]
+  resources :resetvation, only: [:index, :show, :new, :create]
+  resources :inns
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
