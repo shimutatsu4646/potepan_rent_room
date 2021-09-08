ed3b3734dab41464167ce90cdc01764b226e60fe
Ubuntu <ubuntu@ip-172-31-35-1.ap-northeast-1.compute.internal>
2021-09-08 02:01:02 +0000

    検索機能の実装

--- app/controllers/inns_controller.rb
+++ app/controllers/inns_controller.rb
@@ -1,9 +1,9 @@
 class InnsController < ApplicationController
-  before_action :logged_in_user, only:[:new, :create, :edit, :update, :destroy]
-  
+  before_action :logged_in_user, only:[:new, :create, :edit, :update, :destroy, :login_user_index]
+  before_action :set_q, only: [:index, :search]
+
   def index
-    # ログインしているユーザーに関連付いたinnを一覧にする
-    @inns = current_user.inns.all
+    @inns = Inn.all
   end
 
   def show
@@ -13,7 +13,7 @@ class InnsController < ApplicationController
   def new
     @inn = Inn.new
   end
-  
+
   def create
     @inn = current_user.inns.build(inn_params)
     # @inn.image.attach(params[:inn][:image])
@@ -23,15 +23,15 @@ class InnsController < ApplicationController
     else
     # binding.pry
       flash[:alert] = "登録できませんでした。"
-      render "new"      
+      render "new"
     end
   end
-  
+
 
   def edit
     @inn = Inn.find(params[:id])
   end
-  
+
   def update
     @inn = Inn.find(params[:id])
     if @inn.update(inn_params)
@@ -39,18 +39,30 @@ class InnsController < ApplicationController
       redirect_to @inn
     else
       flash[:alert] = "編集できませんでした。"
-      render "edit"     
+      render "edit"
     end
   end
-  
+
   def destroy
     # 関連付いた予約（reservation）が一つもないとき、もしくは
     # 終了日が現在時刻より後のものがないときに削除できるものとする。
   end
-  
+
+  def search
+    @results = @q.result
+  end
+
+  def login_user_index
+    @current_user_inns = current_user.inns.all
+  end
+
   private
-  
+
   def inn_params
     params.require(:inn).permit(:name, :introduction, :price, :adress, :image)
+  end
+
+  def set_q
+    @q = Inn.ransack(params[:q])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  root 'inns#index'
   get 'login', to: "sessions#new"
   post 'login', to: "sessions#create"
   delete 'logout', to: "sessions#destroy"
   
   resources :users
-  resources :inns
+  resources :inns do
+    collection do
+      get :search
+      get :login_user_index
+    end
+  end
   resources :reservations do  
     collection do
       post :confirm
     end
   end
-  
 end
